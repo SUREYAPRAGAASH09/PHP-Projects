1d53269b47544f296e1285d74d16b6bb26eb4e99
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-20 22:12:16 +0530

    intial commit

---
+++ codingBat Problems/frontBack.java
@@ -0,0 +1,30 @@
+Given a string, return a new string where the first and last chars have been exchanged.
+
+frontBack("code") → "eodc"
+frontBack("a") → "a"
+frontBack("ab") → "ba"
+
+public String frontBack(String str) {
+        
+        int Lenght = str.length ();
+        char FirstChar, LastChar, temp;
+        String newString = "";
+        
+        if (Lenght >= 2)
+        {
+    	      char[] ch = str.toCharArray ();
+    	      FirstChar = ch[0];
+    	      LastChar = ch[Lenght - 1];
+    
+    	      ch[0] = LastChar;
+    	      ch[Lenght - 1] = FirstChar;
+    	      for (int i = 0; i <= Lenght-1; i++) { 
+    	         newString = newString + ch[i];
+    	      }  
+      	   return newString;
+        } 
+        else
+        {
+	        return str;
+        }
+}
