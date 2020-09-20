dc3172521a67c292648fa251b0f9df6121bb8ad2
G.Sureya Pragaash <36239801+SUREYAPRAGAASH09@users.noreply.github.com>
2020-09-20 22:13:02 +0530

    intial commit

---
+++ codingBat Problems/front3.java
@@ -0,0 +1,12 @@
+Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+
+front3("Java") → "JavJavJav"
+front3("Chocolate") → "ChoChoCho"
+front3("abc") → "abcabcabc"
+
+public String front3(String str) {
+  if (str.length () >= 3)
+            str = str.substring(0, 3);
+        
+  return str + str + str;
+}
